9eda87b2ef0e0cae5218e49de06a31f926814b0a
RishadPVM <2003rishadap@gmail.com>
2024-12-23 18:18:18 +0530

    sdfsd

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:privacy_policy_web/firebase_options.dart';
 import 'package:privacy_policy_web/page/about.dart';
 import 'package:privacy_policy_web/page/terms_and_conditions.dart';
 import 'package:privacy_policy_web/page/privacy_policy.dart';
 
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(MyApp());
 }
 
